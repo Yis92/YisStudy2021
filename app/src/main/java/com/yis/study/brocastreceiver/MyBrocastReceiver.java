b803a9544bd948a7869d59912632f4913a7cfad7
刘意 <13717833151@163.com>
2021-03-18 22:13:24 +0800

    广播

---
+++ app/src/main/java/com/yis/study/brocastreceiver/MyBrocastReceiver.java
@@ -0,0 +1,16 @@
+package com.yis.study.brocastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by liuyi on 2021-03-01.
+ */
+public class MyBrocastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/com/yis/study/brocastreceiver/MyBrocastReceiverActivity.java
@@ -0,0 +1,20 @@
+package com.yis.study.brocastreceiver;
+
+import com.yis.study.R;
+import com.yis.study.base.BaseActivity;
+
+/**
+ * Created by liuyi on 2021-03-01.
+ */
+public class MyBrocastReceiverActivity extends BaseActivity {
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_my_brocast_receiver;
+    }
+}
